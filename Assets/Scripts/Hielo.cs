bad15c5ef55db258396bb95ee2a1c251eec30f0a
poserkiller <alejandrodasilvaespinosa@gmail.com>
2016-06-07 00:23:43 +0200

    He subido unos audios para las colisiones

--- Assets/Scripts/Hielo.cs
+++ Assets/Scripts/Hielo.cs
@@ -2,13 +2,17 @@ using UnityEngine;
 using System.Collections;
 
 public class Hielo : MonoBehaviour {
-
+	public AudioClip Hieloroto;
+	private AudioSource audio;
 	void OnCollisionEnter(Collision collision){
 		if (GameObject.Find ("balaCannon(Clone)") != null) {
 			if (collision.gameObject.name == "balaCannon(Clone)") {
-				Destroy (gameObject);
+				audio = GetComponent<AudioSource>();
+				audio.clip = Hieloroto;
+				audio.Play();
+				Destroy (gameObject,3f);
+
 			}
 		}
-
 	}
 }
