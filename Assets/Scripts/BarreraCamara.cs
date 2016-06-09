7c6a09d70dad25df15de7224dfe3fde1aee19e1d
poserkiller <alejandrodasilvaespinosa@gmail.com>
2016-06-10 00:15:32 +0200

    He puesto sonido a casi todo a falta de alguna banda sonora o algo que se me haya podido pasar, aparte ahora tambien se puede mover la camara de izquierda a derecha con "a" y "d" y tiene unas barreras que impiden que se mueva mas de lo debido usando triggers.

---
+++ Assets/Scripts/BarreraCamara.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarreraCamara : MonoBehaviour {
+	public bool collision = false;
+	void OnTriggerEnter() {
+		collision = true;
+    }
+		void OnTriggerExit() {
+		collision = false;
+    }
+}
---
+++ Assets/Scripts/CamaraPrincipal.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamaraPrincipal : MonoBehaviour {
+	private BarreraCamara barrera1;
+	private BarreraCamara barrera2;
+	void Awake (){
+		barrera1 = GameObject.Find ("BarreraCamara1").GetComponent<BarreraCamara> ();
+		barrera2 = GameObject.Find ("BarreraCamara2").GetComponent<BarreraCamara> ();
+	}	
+	
+	void Update () {
+		if (barrera1.collision == false){
+		if (Input.GetKey("d")){
+			transform.Translate(Vector3.right * Time.deltaTime * 30);
+		}}
+		if (Input.GetKey("a"))
+		if (barrera2.collision == false){
+		transform.Translate(Vector3.left * Time.deltaTime * 30);}
+	}
+	}
+
